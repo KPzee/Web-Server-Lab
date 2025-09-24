f1284e98b50557e74954c7c3e938ad5a3a7e0812
KPzee <125760357+KPzee@users.noreply.github.com>
2025-09-24 13:10:39 -0400

    PHP stuff

---
+++ form.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form response</title>
+    </head>
+    <body>
+        <p>GET: <?= var_dump($_GET) ?></p>
+        <p>POST: <?= var_dump($_POST) ?></p>
+    </body>
+</html>
