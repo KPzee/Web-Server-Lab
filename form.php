1676c06e67afd9593efa30b48914acea1f2ad19f
KPzee <125760357+KPzee@users.noreply.github.com>
2025-09-24 13:34:56 -0400

    fgdg

--- form.php
+++ form.php
@@ -4,6 +4,6 @@
         <title>Form response</title>
     </head>
     <body>
-        <p>My Favorite tea brand is <?= htmlspecialchars($_POST['teaBrand']) . " and my miswak thickness is " . htmlspecialchars($_POST['miswakThickness']) ?></p>
+        <p>My Favorite tea brand is <?= htmlspecialchars($_POST['teaBrand']) . " and my miswak thickness is " . htmlspecialchars($_POST['miswakThickness']) . " and tea should always be made at" . htmlspecialchars($_POST['teaTemp']) ?></p>
     </body>
 </html>
