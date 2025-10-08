b3d4933e474db06bea4acdbef6e64b22c5d5b38b
KPzee <125760357+KPzee@users.noreply.github.com>
2025-10-08 12:52:25 -0400

    Changed forms or sum added php

---
+++ PHP_Files/response.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form response</title>
+    </head>
+    <?php
+            // Retrieve submitted information
+            $tea_brand = htmlspecialchars($_GET["teaBrand"]);
+            $miswak_thickness = htmlspecialchars($_GET["miswakThickness"]);
+            $tea_temp = htmlspecialchars($_GET["teaTemp"]); 
+            $server = "localhost";
+            $username = "php";
+            $password = "password";
+            $database = "teaType";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            // Check for successful connection
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+            $sql = "INSERT INTO users (type, thick, temp) VALUES ($tea_brand, $miswak_thickness, $tea_temp);";
+            $result = mysqli_query($conn, $sql);
+        ?>
+
+    <body>
+        <p>My Favorite tea brand is <?= htmlspecialchars($_GET['teaBrand']) . " and my miswak thickness is " . htmlspecialchars($_GET['miswakThickness']) . " and tea should always be made at " . htmlspecialchars($_GET['teaTemp']) ?></p>
+    </body>
+</html>
