cb093b6b148903dc24531285b12faa34b0f41524
KPzee <125760357+KPzee@users.noreply.github.com>
2025-11-26 10:32:39 -0500

    test

---
+++ PHP_Files/led.php
@@ -0,0 +1,15 @@
+<?php
+// led.php - Basic PHP page setup
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LED Control</title>
+</head>
+<body>
+    <h1>Welcome to LED Control Page</h1>
+    <p>This is a basic PHP page setup.</p>
+</body>
+</html>
