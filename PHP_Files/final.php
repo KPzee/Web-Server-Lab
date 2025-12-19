82ea66a1f787c9e7761535a467c504429a351d2c
KPzee <125760357+KPzee@users.noreply.github.com>
2025-12-19 11:27:14 -0500

    sas

--- PHP_Files/final.php
+++ PHP_Files/final.php
@@ -6,7 +6,7 @@
     <?php
             // Retrieve submitted information
             $gQuery = htmlspecialchars($_GET["gQuery"]);
-            $server = "localhost";
+            $server = "kpzee.local";
             $username = "php";
             $password = "password";
             $database = "Final";
@@ -20,33 +20,33 @@
             $result = mysqli_query($conn, $sql);
     ?>
 
-    <!-- <body>
-        <p>My Favorite tea brand is <?= htmlspecialchars($_GET['teaBrand']) . " and my miswak thickness is " . htmlspecialchars($_GET['miswakThickness']) . " and tea should always be made at " . htmlspecialchars($_GET['teaTemp']) ?></p>
-                <?php
-                $sql = $sql = "select temp from teaFlavor where temp=75;";
-                $result = mysqli_query($conn, $sql);
-                echo "<h3>The correct temperatures</h3>";
-                    foreach($result as $row) 
-                    {
-                        echo "<p>{$row['temp']}</p>\n";
-                    }
-                $sql = $sql = "select distinct thick from teaFlavor;";
-                $result = mysqli_query($conn, $sql);
-                echo "<h3>The unique thicknesses</h3>";
-                    foreach($result as $row) 
-                    {
-                        echo "<p>{$row['thick']}</p>\n";
-                    }
-                $sql = $sql = "select count(*) from teaFlavor;";
-                $result = mysqli_query($conn, $sql);
-                echo "<h3>How many inputs</h3>";
-                    foreach($result as $row) 
-                    {
-                        echo "<p>{$row['count(*)']}</p>\n";
-                    }
-                    // Don't forget to close the connection!
-                    mysqli_close($conn);
-                ?>
+    <!-- //<body>
+     //   <p>My Favorite tea brand is <?= htmlspecialchars($_GET['teaBrand']) . " and my miswak thickness is " . htmlspecialchars($_GET['miswakThickness']) . " and tea should always be made at " . htmlspecialchars($_GET['teaTemp']) ?></p>
+     //           <?php
+      //          $sql = $sql = "select temp from teaFlavor where temp=75;";
+      //          $result = mysqli_query($conn, $sql);
+      //          echo "<h3>The correct temperatures</h3>";
+      //              foreach($result as $row) 
+      //              {
+      //                  echo "<p>{$row['temp']}</p>\n";
+      //              }
+      //          $sql = $sql = "select distinct thick from teaFlavor;";
+      //          $result = mysqli_query($conn, $sql);
+      //          echo "<h3>The unique thicknesses</h3>";
+      //              foreach($result as $row) 
+      //              {
+      //                  echo "<p>{$row['thick']}</p>\n";
+      //              }
+      //          $sql = $sql = "select count(*) from teaFlavor;";
+      //          $result = mysqli_query($conn, $sql);
+      //          echo "<h3>How many inputs</h3>";
+      //              foreach($result as $row) 
+      //              {
+      //                  echo "<p>{$row['count(*)']}</p>\n";
+      //              }
+      //              // Don't forget to close the connection!
+      //              mysqli_close($conn);
+      //          ?>
 
     </body> -->
 </html>
